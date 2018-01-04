9049ce0e2c69808a8500852cdb2fca2a564b2a2a
Khoi Ngo Nguyen <khoi.ngo@Khois-MacBook-Pro.local>
2018-01-04 16:42:58 +0700

    update code

--- App.jsx
+++ App.jsx
@@ -1,12 +1,40 @@
 import React from 'react';
 
+console.clear();
+
+const Title = ({itemCount}) => {
+   	return (
+      	<h3 className="app--title">Test</h3>
+   	);
+}
+
 class App extends React.Component {
-   render() {
-      return (
-         <div>
-            Hello World!!!
-         </div>
-      );
-   }
+	constructor(props) {
+		super();
+		this.state = {data: []};
+		this.updateState = this.updateState.bind(this);
+	}
+	updateState(e) {
+		this.state.data.push({"numberOfBoxes": e.target.value});
+		console.log(this.state);
+	}
+   	render() {
+      	return (
+      		<div className="app">
+      			<Title />
+      			<InputNumber myDataProp={this.state.data} updateStateProp={this.updateState} />
+        	</div>
+      	);
+   	}
+}
+
+class InputNumber extends React.Component {
+	render () {
+		return (
+			<div className="app--form">
+            	<input type="number" size="50" value= {this.props.myDataProp} onChange={this.props.updateStateProp} />
+	      	</div>
+		);
+	}
 }
 export default App;
